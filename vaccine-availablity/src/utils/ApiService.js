e8281ef242fdd9e17532cd1f0a69c747c4d3ef00
DARSHAN P B <darshanpb111@gmail.com>
2021-05-08 16:34:52 +0530

    Update ApiService.js

--- vaccine-availablity/src/utils/ApiService.js
+++ vaccine-availablity/src/utils/ApiService.js
@@ -1,28 +1,29 @@
 import axios from 'axios';
-const RECAPTCHA = window.location.origin + "/isHuman";
-const VISITOR_DATA  = window.location.origin +'/user';
-export const  validateReCaptcha = (value) =>{
-    axios.get(RECAPTCHA+`/${value}`).then((res)=>{
+const RECAPTCHA = 'http://notifyme-back-end.herokuapp.com' + "/isHuman";
+const URL = 'http://notifyme-back-end.herokuapp.com/user';
+export const validateReCaptcha = (value) => {
+    axios.get(RECAPTCHA + `/${value}`).then((res) => {
         return res.data.success;
-    }).catch((error)=>{
+    }).catch((error) => {
         console.error(error);
         return false;
     });
 };
 
-export const postData = (data) =>{
-    axios.post(VISITOR_DATA,data).then((res)=>{
-        if(res.status === 200)
+export const postData = (data) => {
+    console.log(data)
+    axios.post(URL, data).then((res) => {
+        if (res.status === 200)
             alert(`NO Slots Available currently we will notify you once available...Thank You.`);
-        else if(res.status === 201){
-            if(res.data.message){
+        else if (res.status === 201) {
+            if (res.data.message) {
                 alert(`${res.data.message}`);
             }
-            else{
+            else {
                 alert(``);
             }
         }
-    }).catch((error)=>{
+    }).catch((error) => {
         console.error(error);
     })
 }
