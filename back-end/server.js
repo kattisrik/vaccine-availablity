412b91c1fb9f9024583208fd171f26705e4fa1c5
kattisrik <srikanthkatti.katti@gmail.com>
2021-05-08 17:48:33 +0530

    fixing CORS issue

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const server = express();
+const cors = require('cors');
 
 const body_parser = require("body-parser");
 
+server.use(cors());
 // parse JSON (application/json content-type)
 server.use(body_parser.json());
 
--- vaccine-availablity/src/utils/ApiService.js
+++ vaccine-availablity/src/utils/ApiService.js
@@ -2,6 +2,13 @@ import axios from 'axios';
 const RECAPTCHA = "http://notifyme-back-end.herokuapp.com/isHuman";
 const URL = 'http://notifyme-back-end.herokuapp.com/user';
 
+const instance = axios.create({
+    headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+      }
+  });
+
 export const validateReCaptcha = (value) => {
     axios.get(RECAPTCHA + `/${value}`).then((res) => {
         return res.data.success;
@@ -12,7 +19,7 @@ export const validateReCaptcha = (value) => {
 };
 
 export const postData = (data) => {
-    axios.post(URL, data).then((res) => {
+    instance.post(URL, data).then((res) => {
         if (res.status === 200)
             alert(`NO Slots Available currently we will notify you once available...Thank You.`);
         else if (res.status === 201) {
